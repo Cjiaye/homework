5f0b32e29944fcec43970c2dc760734e2e89edea
Cjiaye <11204115+cjiaye@user.noreply.gitee.com>
2022-07-20 17:04:04 +0800

    docs(all): all

---
+++ src/components/control/image/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <img :src="data[prop]" width="80" height="80" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: () => ({})
+    },
+    config: {
+      type: Object,
+      default: () => ({})
+    },
+    prop: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/control/text/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{ data[prop] }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: () => ({})
+    },
+    config: {
+      type: Object,
+      default: () => ({})
+    },
+    prop: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped></style>
