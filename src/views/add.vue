609bee757b467e68e1b7f957c160b1bd79467b60
Cjiaye <11204115+cjiaye@user.noreply.gitee.com>
2022-07-25 08:14:09 +0800

    docs(all): all

---
+++ src/views/add.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-button>删除</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWorkAdd',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
