bd8857bb7061b7801db56c29a84f1b4b59003ec0
Cjiaye <11204115+cjiaye@user.noreply.gitee.com>
2022-07-29 08:14:23 +0800

    docs(all): all

---
+++ src/views/button.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button>组件封装</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
